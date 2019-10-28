b3a7cc80166ca51b69f4767fa5bc767182a2d67a
Steve Woo <swoo1981@gmail.com>
2019-10-28 14:38:02 -0700

    Switched block id to max

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
+function getClientHeight()
+{
+  return document.compatMode=='CSS1Compat' && !window.opera?document.documentElement.clientHeight:document.body.clientHeight;
+}
+
+var clientHeight = getClientHeight();
+
 $(window).scroll(function() {
     
-    var diff = $("#blockLength").outerHeight() / $("#page").outerHeight();
+    var diff = ($("#blockLength").height() - clientHeight) / ($("#page").height() - clientHeight);
     var blocktoSet = $(window).scrollTop() * diff;
     
     $("#block").scrollTop(blocktoSet);
